5476d2c509b625667172a45212b9c378b4200f5a
shiki <140712093+shiki-01@users.noreply.github.com>
2024-08-13 21:33:29 +0900

    Initial commit

---
+++ src/electron.js
@@ -0,0 +1,133 @@
+import windowStateManager from 'electron-window-state';
+import { app, BrowserWindow, ipcMain } from 'electron';
+import contextMenu from 'electron-context-menu';
+import path from 'path';
+import { initialize, enable } from '@electron/remote/main';
+initialize();
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+if (process.env.NODE_ENV === 'development') {
+	try {
+		require('electron-reloader')(module, {});
+	} catch {
+		console.log('Error: electron-reloaded not found');
+	}
+}
+
+const port = process.env.PORT || 5173;
+const dev = !app.isPackaged;
+let mainWindow;
+
+function createWindow() {
+	let windowState = windowStateManager({
+		defaultWidth: 800,
+		defaultHeight: 600,
+	});
+
+	const mainWindow = new BrowserWindow({
+		backgroundColor: 'whitesmoke',
+		titleBarStyle: 'hidden',
+		autoHideMenuBar: true,
+		trafficLightPosition: {
+			x: 17,
+			y: 32,
+		},
+		minHeight: 450,
+		minWidth: 500,
+		webPreferences: {
+			enableRemoteModule: true,
+			contextIsolation: true,
+			nodeIntegration: true,
+			spellcheck: false,
+			devTools: dev,
+			preload: path.join(__dirname, 'preload.cjs'),
+		},
+		x: windowState.x,
+		y: windowState.y,
+		width: windowState.width,
+		height: windowState.height,
+	});
+
+	enable(mainWindow.webContents);
+
+	windowState.manage(mainWindow);
+
+	mainWindow.once('ready-to-show', () => {
+		mainWindow.show();
+		mainWindow.focus();
+	});
+
+	mainWindow.on('close', () => {
+		windowState.saveState(mainWindow);
+	});
+
+	return mainWindow;
+}
+
+contextMenu({
+	showLookUpSelection: false,
+	showSearchWithGoogle: false,
+	showCopyImage: false,
+	prepend: () => [
+		{
+			label: 'Make App 💻',
+		},
+	],
+});
+
+function loadVite(port) {
+	mainWindow.loadURL(`http://localhost:${port}`).catch((e) => {
+		console.log('Error loading URL, retrying', e);
+		setTimeout(() => {
+			loadVite(port);
+		}, 200);
+	});
+}
+
+function createMainWindow() {
+	mainWindow = createWindow();
+	mainWindow.once('close', () => {
+		mainWindow = null;
+	});
+
+	if (dev) loadVite(port);
+}
+
+app.once('ready', () => {
+	createMainWindow();
+});
+app.on('activate', () => {
+	if (!mainWindow) {
+		createMainWindow();
+	}
+});
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') app.quit();
+});
+
+ipcMain.on('to-main', (event, count) => {
+	return mainWindow.webContents.send('from-main', `next count is ${count + 1}`);
+});
+
+// window
+ipcMain.on('window-close', () => {
+	mainWindow.close();
+});
+ipcMain.on('window-minimize', () => {
+	mainWindow.minimize();
+});
+ipcMain.on('window-maximize', () => {
+	if (mainWindow.isMaximized()) {
+		mainWindow.unmaximize();
+	} else {
+		mainWindow.maximize();
+	}
+});
